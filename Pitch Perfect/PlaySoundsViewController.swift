2147ea5dde3baea71f4673daf2b214ab624d2438
Lincoln Sea <sealincoln@gmail.com>
2015-07-08 22:52:46 -0700

    Add slow sound button

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Lincoln Sea on 7/8/15.
+//  Copyright (c) 2015 Lincoln Sea. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBAction func slowSound(sender: UIButton) {
+    }
+}
